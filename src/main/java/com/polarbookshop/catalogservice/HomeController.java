cc2aa676185c571e6b4d26442a6bf4177ed83452
todokaima <thodoroskaimasidis@gmail.com>
2026-07-04 15:19:19 +0300

    configuration parameterization

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -2,10 +2,12 @@ package com.polarbookshop.catalogservice;
 
 
 import com.polarbookshop.catalogservice.config.PolarProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HomeController {
     private final PolarProperties polarProperties;
 
